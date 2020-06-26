b1c53ab1eb774cc4dab261f5f1c09c7f48c5b458
conaldev <talaaidoaibiet@gmail.com>
2020-06-26 13:42:12 +0700

    init class character & images

---
+++ javascript/classCharacter.js
@@ -0,0 +1,26 @@
+function Character (map,options) {
+    if(!options)
+        options = {};
+    this.map = map;
+    this.left = options.left || 0;
+    this.top = options.top || 300;
+    this.height = options.height || 20;
+    this.width = options.width || 20;
+
+    this.jumpPower = options.jumpPower || 4;
+    this.speed = options.speed || 1;
+    this.speedX = (Math.random() >= 0.5 ? this.speed : -this.speed);
+    this.speedY = 0;
+
+    this.isJumping = true;
+    this.isDead = false;
+
+    this.bottom = this.top + this.height;
+    this.right = this.left + this.width;
+
+    this.canDestroyObstacles = options.canDestroyObstacles
+    this.isAutoMoving = options.isAutoMoving;
+    this.canJump = options.canJump;
+    this.dieOnCollide = options.dieOnCollide;
+
+}
