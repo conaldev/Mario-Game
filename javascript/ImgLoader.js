ffda8ae36e3b27567970d9c47e4134ca06ef86bb
conaldev <talaaidoaibiet@gmail.com>
2020-06-29 07:52:03 +0700

    add img load

---
+++ javascript/ImgLoader.js
@@ -0,0 +1,26 @@
+function ImgLoader(sources, onProgressChanged, onCompleted) {
+    this.images = {};
+    let loadedImages = 0;
+    let totalImages = 0;
+    if(onProgressChanged || onCompleted)
+        for(let src in sources) {
+            totalImages++;
+        }
+    let self = this;
+    for(let src in sources) {
+        this.images[src] = new Image();
+        this.images[src].onload = function () {
+            loadedImages++;
+            if (onProgressChanged) {
+                let percent = Math.floor((loadedImages/totalImages) *100);
+                onProgressChanged(this,percent);
+            }
+            if(onCompleted && loadedImages >= totalImages)
+                onCompleted(self.images);
+        };
+        this.images[src].onerror = function (e) {
+            console.log(src);
+        };
+        this.images[src].src = sources[src];
+    }
+}
